a8f19d497448987ef752208e5173fd7518ced36f
shenningsgard <steve@stevehenningsgard.com>
2015-04-02 16:34:18 -0500

    Better submit.php feedback

--- submit.php
+++ submit.php
@@ -1,7 +1,8 @@
 <?php
+    $name = $email = $subject = $description = "";
 
     if ( isset($_POST['email']) ) {
-        $name = $email = $subject = $description = "";
+
 
         $name = clean_input( $_POST['name'] );
         $email = clean_input( $_POST['email'] );
@@ -42,8 +43,57 @@
     }
 
 ?>
+<!DOCTYPE html>
 <html>
-    <body>
-        Sent! Talk to you soon! - CodeHelp Team
-    </body>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CodeHelp</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <style>
+        .header {
+            color: #c10b12;
+            font-size: 27px;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="well well-sm">
+                <form class="form-horizontal" method="post">
+                    <fieldset>
+                        <legend class="text-center header">Thank you for using CodeHelp :)</legend>
+                        <h3 class="text-center ">Here's what you sent us.</h3>
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-2">
+                                <input id="name" name="name" type="text" placeholder="Name" class="form-control" value="<?php echo $name ?>" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-2">
+                                <input id="email" name="email" type="text" placeholder="Email Address" class="form-control" value="<?php echo $email ?>" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-2">
+                                <input id="subject" name="subject" type="text" placeholder="Class Subject" class="form-control" value="<?php echo $subject ?>" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-2">
+                                <textarea class="form-control" id="description" name="description" placeholder="Describe the problem you are encountering. We will get back to you within 2 hours." rows="7" value="<?php echo $message ?>" readonly></textarea>
+                            </div>
+                        </div>
+                    </fieldset>
+                    <h3 class="text-center">We will contact you shortly.</h3>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
 </html>
