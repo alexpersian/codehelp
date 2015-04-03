0077d2dacc732679c6745e48ec7f6765cd2884c3
shenningsgard <steve@stevehenningsgard.com>
2015-04-02 19:06:40 -0500

    Pretty background

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
             <div class="well well-sm">
                 <form class="form-horizontal" method="post" action="submit.php">
                     <fieldset>
-                        <legend class="text-center header">Welcome to CodeHelp<br /><small class="subheading">a free service for SCSU students!</small></legend>
+                        <legend class="text-center header">Welcome to CodeHelp<br /><small class="subheading">A service for SCSU students!</small></legend>
                         <div class="form-group">
                             <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
                             <div class="col-md-6">
