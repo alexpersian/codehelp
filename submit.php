36a0ad08847b931c335609c914eb819fbbefbd91
shenningsgard <steve@stevehenningsgard.com>
2015-04-03 10:47:44 -0500

    StackExchange working/about to be removed

--- submit.php
+++ submit.php
@@ -85,6 +85,9 @@
             -o-background-size: cover;
             background-size: cover;
         }
+        .media {
+            font-size: 10pt;
+        }
 
     </style>
     </head>
@@ -139,8 +142,8 @@
                     <h3 class="text-center">...We'll <?php echo ($error_text != "" || $success == false) ? "try to " : "" ?>contact you shortly!</h3>
                 </form>
             </div>
-            <?php include("templates/include/createdby.php"); ?>
         </div>
+        <?php include("templates/include/createdby.php"); ?>
     </div>
 </div>
 </body>
@@ -150,7 +153,6 @@
 <script type="text/javascript">
     $(document).ready(function() {
         var content = $("#main-content");
-
         var searchString = <?php echo "\"" . $message . "\"" ?>;
         var tags = ( (<?php echo "\"" . $language . "\"" ?>).replace("(other)","") ).split("/");
 
@@ -159,36 +161,51 @@
             tags: tags
         };
 
+        //THIS IS THE CALL TO StackExchange!
         SE.get(config, printResponse);
 
-        console.log(searchString);
-        console.log(tags);
 
+        //prints the response as Bootstrap media objects, after the 'main-content' div
         function printResponse(resp) {
             if (resp.items) {
+
+
                 $.each(resp.items, function(i, item) {
                     var mediaObject;
 
+                    /*
+                    var divMedia = $("<div>").addClass("media");
+                    var divMediaLeft = $("<div>").addClass("media-left");
+                    var linky = $("<a>").attr("href", item.link);
+                    var imgMedia = $("<img>").attr("src", item.owner.profile_image);
+                    var divMediaBody = $("<div>").addClass("media-body").html( (item.body).substring(0, 250) + "..." );
+                    var h4mediaHeading = $("<h4>").addClass("media-heading").html(item.title);
+                    */
+
+
                     if (item.is_answered) {
-                        mediaObject = "<div class=\"media\">" +
-                        "<div class=\"media-left\"" +
-                        "<a href=\"" + item.link + "\">" +
-                        "<img class=\"media-object\" src=\"" + item.owner.profile_image + "\">" +
-                        "</a>" +
-                        "</div>" +
-                        "<div class=\"media-body\">" +
-                        "<h4 class=\"media-heading\">" + item.title + "</h4>" +
-                        (item.body).substring(0,100) + "..." +
-                        "</div>" +
-                        "</div>";
+                        mediaObject = $("<div>").addClass("media").addClass("col-md-6").append(
+                            $("<div>").addClass("media-left").append(
+                                $("<a>").attr("href", item.link).attr("target","_blank").append(
+                                    $("<img>").attr("src", item.owner.profile_image).attr("width", "32px")
+                                )
+                            )
+                        ).append(
+                            $("<div>").addClass("media-body").html( (item.body).substring(0, 250) + "...").prepend(
+                                $("<a>").attr("href", item.link).attr("target","_blank").append(
+                                    $("<h4>").addClass("media-heading").html(item.title)
+                                )
+                            )
+                        )
 
                         content.after(mediaObject);
                     } else {
                         console.log("answer count too low", item);
                     }
 
+                });
 
-                })
+                content.after( $("<h3 class=\"col-md-10\">").html("While you're waiting, perhaps these resources might help?") );
             } else {
                 console.log(resp);
             }
